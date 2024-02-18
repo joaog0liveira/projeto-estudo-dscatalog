0fd50e0ac66bf5dd674bbe840de72fc611783078
João Gabriel <118997962+joaog0liveira@users.noreply.github.com>
2024-02-18 10:18:24 -0300

    backend project created

---
+++ backend/src/test/java/br/com/joaogabriel/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaogabriel.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
